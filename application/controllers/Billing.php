61ed68591d28dd140df19295104bbb3eefb823a5
Kenji Prahyudi <kenji.prahyudi@gmail.com>
2017-12-01 18:50:05 +0700

    revert billing controller

--- application/controllers/Billing.php
+++ application/controllers/Billing.php
@@ -1,24 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Billing extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index($id)
+	
+	public function index()
 	{
 		// Load Header
         $data_header['css_list'] = array();
@@ -26,12 +10,43 @@ class Billing extends CI_Controller {
 		$this->load->view('header', $data_header);
 		
 		// Load Body
-		$data['echo'] = "Daftar Billing";
 		$data['model'] = new class{};
-		$this->load->view('tenant/billing', $data);
+		$data['title'] = "Daftar Billing";
+		
+		if ($this->session->userdata('username') == "tenant") // dummy
+		{
+			$this->load->view('tenant/billing_list', $data);
+		}
+		else
+		{
+			$this->load->view('billing_list', $data);
+		}
 		
 		// Load Footer
 		$this->load->view('footer');
 	}
-
+	
+	public function detail($id)
+	{
+		// Load Header
+        $data_header['css_list'] = array();
+        $data_header['js_list'] = array();
+		$this->load->view('header', $data_header);
+		
+		// Load Body
+		$data['title'] = "Billing";
+		$data['model'] = new class{};
+		
+		if ($this->session->userdata('username') == "tenant") // dummy
+		{
+			$this->load->view('tenant/billing', $data);
+		}
+		else
+		{
+			$this->load->view('billing', $data);
+		}
+		
+		// Load Footer
+		$this->load->view('footer');
+	}
 }
