10ecf8f432331111433f10596b65315cde590322
Kenji Prahyudi <kenji.prahyudi@gmail.com>
2017-12-13 23:34:20 +0700

    add text renderer library, search feature

--- application/libraries/Id_Generator.php
+++ application/libraries/Id_Generator.php
@@ -3,20 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Id_Generator {
 
-	// const INITIAL = array(
-		// "CUSTOMER" => "CUS",
-		// "TENANT" => "TNT",
-		// "DELIVERER" => "DLV",
-		// "ADMIN" => "ADM"
-	// );
-
-	// const ID_LENGTH = array(
-		// "CUSTOMER" => 8,
-		// "TENANT" => 5,
-		// "DELIVERER" => 5,
-		// "ADMIN" => 5
-	// );
-
 	public function generate($type, $id)
 	{
 		return TYPE['initial'][$type] . str_pad($id, TYPE['id_length'][$type], '0', STR_PAD_LEFT);
--- application/models/views/Search_view_model.php
+++ application/models/views/Search_view_model.php
@@ -1,6 +1,6 @@
 <?php
 
-class Search_view_model {
+class Search_view_model extends CI_Model{
 	
 	public $search_items;
 	
@@ -12,12 +12,13 @@ class Search_view_model {
 	
 	public function get($items)
 	{
+		$this->load->library('text_renderer');
 		foreach ($items as $item)
 		{
 			$temp = new class{};
 			$temp->id = $item->id;
 			$temp->posted_item_name = $item->posted_item_name;
-			$temp->price = $item->price;
+			$temp->price = $this->text_renderer->to_rupiah($item->price);
 			$temp->image_one_name = $item->image_one_name;
 			
 			$this->search_items[] = $temp;
