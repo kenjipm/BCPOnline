e5428c39aff55228f36a3f02dcd592bc80b73d0d
levanjipm <myc.glace@gmail.com>
2018-04-25 00:51:19 +0700

    Item maksa

--- application/views/search.php
+++ application/views/search.php
@@ -1,35 +1,21 @@
-<?php
 
-	// Model Dummy Untuk Search Item List
-	// $model->search_items = array();
-	
-	// $model->search_items[0] = new class{};
-	// $model->search_items[0]->id = 1;
-	// $model->search_items[0]->name = "Charger Samsung";
-	// $model->search_items[0]->price = "Rp 250.000";
-	// $model->search_items[0]->image_one_name = site_url("img/upload/user1/charger_samsung.jpg");
-	// $model->search_items[1] = new class{};
-	// $model->search_items[1]->id = 2;
-	// $model->search_items[1]->name = "Charger Wireless";
-	// $model->search_items[1]->price = "Rp 350.000";
-	// $model->search_items[1]->image_one_name = site_url("img/upload/user1/wireless_samsung.jpg");
-
-	?>
 <!-- left bar buat category -->
 <div class="cb-row">
 	<div class="cb-col-fifth">
-		<div class="panel-category">
-			<h3 class= "cb-txt-primary-1 cb-font-title">KATEGORI</h3>
-			<div class="panel-category-heading">
+		<div class="cb-p-5">
+			<div class= "cb-txt-primary-1 cb-font-title cb-font-size-xl cb-align-center cb-pb-2">KATEGORI</div>
+			<div class="cb-border-round cb-bg-primary-1 cb-pl-5 cb-pr-5 cb-pb-5">
 			<?php
 			foreach($model->categories as $category)
 			{
 				?>
-				<a href="<?=site_url('item/category/'.$category->id)?>" class="category">
-					<div class="panel-category-body">
-						<?=$category->category_name?>
-					</div>
-				</a>
+				<div class="cb-row cb-border-bottom cb-border-white">
+					<a href="<?=site_url('item/category/'.$category->id)?>" class="category cb-col-full cb-font-primary-2">
+						<div class="panel-body">
+							<?=$category->category_name?>
+						</div>
+					</a>
+				</div>
 				<?php
 			}
 			?>
@@ -37,80 +23,79 @@
 		</div>
 	</div>
 	<div class="cb-col-fifth-4">
-		<div class="cb-panel">
-			<div class="cb-panel-heading cb-align-center">
-				<h3 class="cb-txt-primary-1 cb-font-title">PRODUK LAINNYA</h3>
-			</div>
-			<div class="cb-panel-body cb-p-2">
-				<div class="item-showcase cb-row">
-					<?php
-						foreach($model->promoted_items as $promoted_item)
-						{
-							?>
-							<a href="<?=site_url('item/'.$promoted_item->id)?>" class="cb-col-fourth cb-pb-3">
-								<div class="item_thumbnail cb-border-round">
-									<div class="item_photo">
-										<img src="<?=$promoted_item->image_one_name?>" alt="<?=$promoted_item->posted_item_name?>"/>
-									</div>
-									<div class="item_tenant_name">
-									</div>
-									<div class="item_name">
-										*<?=$promoted_item->posted_item_name?>
-									</div>
-									<div class="item_initial_price">
-										
-									</div>
-									<div class="item_current_price">
-										<?=$promoted_item->price?>
-									</div>
-									<div class="item_rating cb-row cb-vertical-center cb-align-center">
-										<span class="cb-star cb-star-<?=$promoted_item->rating->rating_average_round?>"></span>
-										<span class="cb-ml-2">(<?= $promoted_item->rating->rating_count ?>)</span>
-									</div>
-								</div>
-							</a>
-							<?php
-						}
-					?>
-					<?php
-						foreach($model->search_items as $search_item)
-						{
-							?>
-							<a href="<?=site_url('item/'.$search_item->id)?>" class="cb-col-fourth cb-pb-3">
-								<div class="item_thumbnail cb-border-round">
-									<div class="item_photo">
-										<img src="<?=$search_item->image_one_name?>" alt="<?=$search_item->posted_item_name?>"/>
-									</div>
-									<div class="item_tenant_name">
-									</div>
-									<div class="item_name">
-										<?=$search_item->posted_item_name?>
-									</div>
-									<div class="item_initial_price">
-										
-									</div>
-									<div class="item_current_price">
-										<?=$search_item->price?>
-									</div>
-									<div class="item_rating cb-row cb-vertical-center cb-align-center">
-										<span class="cb-star cb-star-<?=$search_item->rating->rating_average_round?>"></span>
-										<span class="cb-ml-2">(<?= $search_item->rating->rating_count ?>)</span>
-									</div>
-								</div>
-							</a>
-							<?php
-						}
-						if (count($model->search_items) <= 0)
-						{
-							?>
-							<div class="col-md-4">
-								<label>Hasil pencarian tidak ditemukan</label>
-							</div>
-							<?php
-						}
+		<div class="cb-p-5">
+			<div class="cb-txt-primary-1 cb-font-title cb-font-size-xl cb-align-center">PRODUK</div>
+		
+			<div class="item-showcase cb-row">
+				<?php
+					foreach($model->promoted_items as $promoted_item)
+					{
 						?>
+						<a href="<?=site_url('item/'.$promoted_item->id)?>" class="cb-col-fourth cb-pb-3">
+							<div class="item_thumbnail cb-border-round">
+								<div class="item_photo">
+									<img src="<?=$promoted_item->image_one_name?>" alt="<?=$promoted_item->posted_item_name?>"/>
+								</div>
+								<div class="item_tenant_name">
+								</div>
+								<div class="item_name">
+									*<?=$promoted_item->posted_item_name?>
+								</div>
+								<div class="item_initial_price">
+									
+								</div>
+								<div class="item_current_price">
+									<?=$promoted_item->price?>
+								</div>
+								<div class="item_rating cb-row cb-vertical-center cb-align-center">
+									<span class="cb-star cb-star-<?=$promoted_item->rating->rating_average_round?>"></span>
+									<span class="cb-ml-2">(<?= $promoted_item->rating->rating_count ?>)</span>
+								</div>
+							</div>
+						</a>
+						<?php
+					}
+				?>
+				<?php
+					foreach($model->search_items as $search_item)
+					{
+						?>
+						<a href="<?=site_url('item/'.$search_item->id)?>" class="cb-col-fourth cb-pb-3">
+							<div class="item_thumbnail cb-border-round">
+								<div class="item_photo">
+									<img src="<?=$search_item->image_one_name?>" alt="<?=$search_item->posted_item_name?>"/>
+								</div>
+								<div class="item_tenant_name">
+								</div>
+								<div class="item_name">
+									<?=$search_item->posted_item_name?>
+								</div>
+								<div class="item_initial_price">
+									
+								</div>
+								<div class="item_current_price">
+									<?=$search_item->price?>
+								</div>
+								<div class="item_rating cb-row cb-vertical-center cb-align-center">
+									<span class="cb-star cb-star-<?=$search_item->rating->rating_average_round?>"></span>
+									<span class="cb-ml-2">(<?= $search_item->rating->rating_count ?>)</span>
+								</div>
+							</div>
+						</a>
+						<?php
+					}
+					if (count($model->search_items) <= 0)
+					{
+						?>
+						<div class="cb-panel-body cb-bg-primary-3 cb-p-5 cb-mt-2">
+							<div class="cb-row">
+								<div class="cb-label">Hasil pencarian tidak ditemukan</div>
+							</div>
+						</div>
+						<?php
+					}
 					?>
-				</div>
+				?>
 			</div>
 		</div>
 	</div>
