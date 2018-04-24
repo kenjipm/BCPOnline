c310145b3e1cb648606a86868e391de8555d2306
Kenji Prahyudi <kenji.prahyudi@gmail.com>
2018-04-24 23:53:19 +0700

    fix default item image favorite

--- application/models/views/customer/Favorite_item_view_model.php
+++ application/models/views/customer/Favorite_item_view_model.php
@@ -24,7 +24,7 @@ class Favorite_item_view_model extends CI_Model {
 			
 			$favorite_item_temp->posted_item 					= new class{};
 			$favorite_item_temp->posted_item->id				= $favorite_item->posted_item->id;
-			$favorite_item_temp->posted_item->image_one_name	= site_url($favorite_item->posted_item->image_one_name);
+			$favorite_item_temp->posted_item->image_one_name	= site_url(($favorite_item->posted_item->image_one_name != "") ? $favorite_item->posted_item->image_one_name : DEFAULT_ITEM_PICTURE[$favorite_item->posted_item->item_type]);
 			$favorite_item_temp->posted_item->posted_item_name	= $favorite_item->posted_item->posted_item_name;
 			$favorite_item_temp->posted_item->price				= $this->text_renderer->to_rupiah($favorite_item->posted_item->price);
 			$favorite_item_temp->rating				= $favorite_item->posted_item->calculate_rating();
