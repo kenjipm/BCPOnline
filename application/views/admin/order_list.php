7115e68266ae79fa87666d39a79a0b50f562ac3a
levanjipm <myc.glace@gmail.com>
2018-04-28 19:37:35 +0700

    Pengiriman barang admin

--- application/views/admin/order_list.php
+++ application/views/admin/order_list.php
@@ -1,3 +1,74 @@
+<div class="cb-col-full cb-txt-primary-1 cb-font-title">
+	<div class="cb-align-center cb-font-size-xl">DAFTAR ORDER</div>
+</div>
+<form action="<?=site_url('order/order_list')?>" class="form-horizontal" method="post">
+	<div class="cb-panel-body cb-bg-primary-3 cb-m-5 cb-p-5">
+		<div class="cb-row">
+			<div class="cb-col-tenth">
+			</div>
+			<div class="cb-col-fifth">
+				<div class="cb-label cb-font-title cb-align-center"> Barang </div>
+			</div>
+			<div class="cb-col-tenth-3">
+				<div class="cb-label cb-font-title cb-align-center"> Alamat </div>
+			</div>
+			<div class="cb-col-fifth">
+				<div class="cb-label cb-font-title cb-align-center"> Metode Pengiriman </div>
+			</div>
+			<div class="cb-col-fifth">
+				<div class="cb-label cb-font-title cb-align-center"> Pengirim </div>
+			</div>
+		</div>
+		<?php
+		foreach($model->order_list as $order)
+		{
+			?>
+			<input type="hidden" name="order_id[]" value="<?=$order->id?>" readonly/>
+			<input type="hidden" name="tenant_id[]" value="<?=$order->tenant_id?>" readonly/>
+			<input type="hidden" name="customer_id[]" value="<?=$order->customer_id?>" readonly/>
+			<input type="hidden" name="item_type[]" value="<?=$order->item_type?>" readonly/>
+			
+			<div class="cb-row cb-p-5 cb-border-top cb-table-striped">
+				<div class="cb-col-tenth">
+					<div class=" cb-align-center"><?=$order->item_type?> </div>
+				</div>
+				<div class="cb-col-fifth">
+					<div class=" cb-align-center"> <?=$order->posted_item?> </div>
+				</div>
+				<div class="cb-col-tenth-3">
+					<div class="cb-align-center"> <?=$order->address?> </div>
+				</div>
+				<div class="cb-col-fifth">
+					<div class="cb-align-center"> <?=$order->collection_method?> </div>
+				</div>
+				<div class="cb-col-fifth">
+					<select class="cb-input-select cb-row cb-col-full" name="deliverer_id[]">
+					<?php
+					foreach ($model->idle_deliverer as $deliverer)
+					{
+						?>
+						<option value="<?=$deliverer->id?>"><?=$deliverer->name?></option>			
+						<?php
+						$i++;
+					}
+					?>
+					</select>
+				</div>
+			</div>
+			<?php
+		}
+		?>
+		<div class="cb-row">
+			<div class="cb-col-tenth-9">
+			</div>
+			<div class="cb-col-tenth">
+				<button type="submit" class="cb-button-form">KIRIM</button>
+			</div>
+		</div>
+	</div>
+</form>
+
+<?php /*
 <div class="col-sm-10 col-sm-offset-1">
 	<div class="panel panel-default">
 		<div class="panel-heading">
@@ -64,3 +135,4 @@
 		</div>
 	</div>
 </div>
+*/ ?>
