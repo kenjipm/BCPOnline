7e57667eadf72e137a9283dac24ad7bc2bb36c2a
levanjipm <myc.glace@gmail.com>
2018-05-01 20:35:01 +0700

    Repair Customer FrontEnd

--- application/views/customer/repair_detail.php
+++ application/views/customer/repair_detail.php
@@ -1,3 +1,50 @@
+<div class="cb-row cb-p-5">
+	<div class="cb-col-full cb-txt-primary-1 cb-align-left cb-font-title cb-font-size-xl">
+		DAFTAR BARANG
+	</div>
+	<div class="cb-col-full cb-row cb-panel-body cb-bg-primary-3 cb-p-5">
+	<?php
+	if ($model->transaction_detail->item_type == "ORDER")
+	{
+		?>
+		<div class="cb-col-full cb-row cb-mb-5">
+			<div class="cb-col-fifth cb-label cb-txt-primary-1">
+				<div class="cb-pull-left">Nama Barang</div>
+				<div class="cb-pull-right">:</div>
+			</div>
+			<div class="cb-col-fifth-2 cb-pl-3">
+				<input type="text" class="cb-row cb-col-full cb-input-text" id="item_name" value="<?=$model->repair_list[0]->otp?>" readonly>
+			</div>
+		</div>
+		<div class="cb-col-full cb-row cb-mb-5">
+			<div class="cb-col-fifth cb-label cb-txt-primary-1">
+				<div class="cb-pull-left">Deliverer</div>
+				<div class="cb-pull-right">:</div>
+			</div>
+			<div class="cb-col-fifth-2 cb-pl-3">
+				<input type="text" class="cb-row cb-col-full cb-input-text" id="deliverer_name" value="<?=$model->repair_list[0]->deliverer_name?>" readonly>
+			</div>
+		</div>
+		<?php
+		foreach($model->repair_list as $repair)
+		{
+			?>
+			<div class="cb-col-full cb-row cb-mb-5">
+				<div class="cb-col-fifth-4 cb-pl-3">
+					<?=$repair->posted_item_description?> </div>
+				<div class="cb-col-fifth cb-pl-3">
+					<a class="cb-button-form" href="<?=site_url('Order/transaction_detail/'.$repair->id)?>">
+						Lihat
+					</a>
+				</div>
+			</div>
+			<?php
+		}
+		?>
+	</div>
+</div>
+
+<?php /*
 <div class="col-sm-10 col-sm-offset-1">
 	<div class="panel panel-default">
 		<div class="panel-heading">
@@ -46,3 +93,5 @@
 		</div>
 	</div>
 </div>
+
+*/ ?>
--- application/views/customer/repair_list.php
+++ application/views/customer/repair_list.php
@@ -1,3 +1,20 @@
+<form action="<?=site_url('order/order_list')?>" method="post">
+	<div class="cb-row cb-p-5">
+		<div class="cb-col-full cb-row cb-border-round cb-bg-primary-2 cb-pl-5 cb-pr-5 cb-pb-5 cb-pt-2 cb-align-center">
+			<div class="cb-font-title cb-txt-primary-3 cb-font-size-xl">MASUKKAN OTP</div>
+			<div class="cb-col-full cb-row cb-border-round cb-bg-primary-3 cb-p-5">
+				<div class="cb-col-full cb-row cb-mb-5">
+					<input type="text" class="cb-input-text cb-col-full cb-align-center" name="otp"/>
+				</div>
+				<div class="cb-col-full cb-row cb-align-center">
+					<button class="cb-button cb-button-form cb-col-fifth" type="submit">KIRIM</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</form>
+
+<?php /*
 <div class="col-sm-10 col-sm-offset-1">
 	<div class="panel panel-default">
 		<div class="panel-heading">
@@ -20,3 +37,4 @@
 		</div>
 	</div>
 </div>
+*/ ?>
