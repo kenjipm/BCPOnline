ee2001980dc87939606a9c1e7b2e61c19803d377
levanjipm <myc.glace@gmail.com>
2018-06-06 11:55:56 +0700

    Redeem Reward

--- application/views/customer/repair_detail.php
+++ application/views/customer/repair_detail.php
@@ -4,7 +4,7 @@
 	</div>
 	<div class="cb-col-full cb-row cb-panel-body cb-bg-primary-3 cb-p-5">
 	<?php
-	if ($model->transaction_detail->item_type == "ORDER")
+	if ($model->transaction_detail->item_type == "REPAIR")
 	{
 		?>
 		<div class="cb-col-full cb-row cb-mb-5">
@@ -40,58 +40,7 @@
 			</div>
 			<?php
 		}
-		?>
+	}
+	?>
 	</div>
 </div>
-
-<?php /*
-<div class="col-sm-10 col-sm-offset-1">
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h3>Daftar Barang untuk Dikirim</h3>
-		</div>
-		<div class="panel-body">
-			<form class="form-horizontal">
-				<div class="form-group">
-					<label class="control-label col-xs-3" for="otp">OTP:</label>
-					<div class="col-xs-9"><input type="text" class="form-control" 
-						value="<?=$model->repair_list[0]->otp?>" readonly></div>
-				</div>
-				<div class="form-group">
-					<label class="control-label col-xs-3" for="deliverer_name">Deliverer:</label>
-					<div class="col-xs-9"><input type="text" class="form-control" 
-						value="<?=$model->repair_list[0]->deliverer_name?>" readonly></div>
-				</div>
-				<div class="form-group">
-					<label class="control-label col-xs-3" for="orders">Repair List:</label>
-					<div class="col-xs-9 pull-right">
-						<div class="row list-group">
-							<div class="col-xs-4">Nama </div>
-						</div>
-					</div>
-					<div class="row">
-					<?php
-					foreach($model->repair_list as $repair)
-					{
-						?>
-						<div class="col-xs-9 pull-right">
-							<div class="row list-group">
-								<div class="col-xs-4 list-group-item">
-									<?=$repair->posted_item_description?> </div>
-								<div class="col-xs-1">
-									<a class="btn btn-default" href="<?=site_url('Order/transaction_detail/'.$repair->id)?>">
-										Lihat
-									</a></div>
-							</div>
-						</div>
-						<?php
-					}
-					?>
-					</div>
-				</div>
-			</form>
-		</div>
-	</div>
-</div>
-
-*/ ?>
