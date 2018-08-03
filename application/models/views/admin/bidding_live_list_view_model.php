8b0928f3a5e2165917843abfcd48a367d8879453
levanjipm <myc.glace@gmail.com>
2018-08-03 14:47:37 +0700

    Semua bidding

--- application/models/views/admin/bidding_live_list_view_model.php
+++ application/models/views/admin/bidding_live_list_view_model.php
@@ -12,24 +12,32 @@ class Bidding_live_list_view_model extends CI_Model {
 	{
 		parent::__construct();
 		
-		$this->item = new class{};
+		$this->items = array();
 		$this->biddings = array();
 		$this->active_flash = false;
 		$this->active_bid = false;
 	}
 	
-	public function get($item, $biddings, $active_flash)
+	public function get($items, $biddings, $active_flash)
 	{
 		$i = 0;
 		$this->load->library('text_renderer');
-
-		$this->item->id 				= $item->id;
-		$this->item->image_one_name		= site_url($item->image_one_name);
-		$this->item->posted_item_name 	= $item->posted_item_name;
-		$this->item->posted_item_description 	= $item->posted_item_description;
-		$this->item->is_confirmed 		= $item->is_confirmed;
-		$this->item->is_winner	 		= $item->is_winner;
 		
+		foreach ($items as $item)
+		{
+			$this->items[$i] = new class{};
+			// print_r($item);
+			$this->items[$i]->id 				= $item->id;
+			$this->items[$i]->image_one_name	= site_url($item->image_one_name);
+			$this->items[$i]->posted_item_name 	= $item->posted_item_name;
+			$this->items[$i]->posted_item_description 	= $item->posted_item_description;
+			$this->items[$i]->is_confirmed 		= $item->is_confirmed;
+			if ($i == 0) $this->items[$i]->is_winner = $item->is_winner;
+			
+			$i++;
+		}
+		
+		$i = 0;
 		foreach ($biddings as $bidding)
 		{	
 			$this->biddings[$i] = new class{};
@@ -39,6 +47,8 @@ class Bidding_live_list_view_model extends CI_Model {
 			$this->biddings[$i]->bid_time		= $bidding->bid_time;
 			$this->biddings[$i]->bid_price		= $bidding->bid_price;
 			$this->biddings[$i]->posted_item_id	= $bidding->posted_item_id;
+			
+			$i++;
 		}
 		
 		if ($active_flash) 
@@ -46,11 +56,11 @@ class Bidding_live_list_view_model extends CI_Model {
 			$this->active_flash = true;
 			$this->active_bid = false;
 		}
-		if (($item) &&  ($this->item->is_confirmed))
+		if (($items) &&  ($this->items[0]->is_confirmed))
 		{
 			$this->active_flash = false;
 			$this->active_bid = true;
-			if ($this->item->is_winner)
+			if ($this->items[0]->is_winner)
 			{
 				$this->active_bid = false;
 			}
