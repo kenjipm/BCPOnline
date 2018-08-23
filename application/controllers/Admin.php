dd600bf098154404e4f43a2056ab825dab04bfbb
levanjipm <myc.glace@gmail.com>
2018-08-23 12:22:13 +0700

    Form validation

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -135,22 +135,14 @@ class Admin extends CI_Controller {
 			$this->form_validation->set_rules('posted_item_name', 'Nama', 'required');
 			$this->form_validation->set_rules('price', 'Harga awal', 'required|integer');
 			$this->form_validation->set_rules('promo_price', 'Harga promo', 'required|integer');
+			$this->form_validation->set_rules('quantity_available', 'Jumlah Stok', 'integer');
 			$this->form_validation->set_rules('unit_weight', 'Berat', 'integer');
 			$this->form_validation->set_rules('posted_item_description', 'Deskripsi', 'required');
+			// $this->form_validation->set_rules('var_desc', 'Deskripsi Varian', 'string');
 			$this->form_validation->set_rules('date_expired', 'Tanggal Berlaku', 'required');
 			$this->form_validation->set_rules('category_id', 'Kategori', 'required');
 			$this->form_validation->set_rules('brand_id', 'Brand', 'required');
 			
-			$variance = $this->input->post('var_desc');
-			$quantity = $this->input->post('quantity_available');
-			for($i = 0; $i < count($variance); $i++) 
-			{
-				$var_desc = $variance[$i];
-				$quantity_available = $quantity[$i];
-
-				$this->form_validation->set_rules($var_desc, "Deskripsi Varian", "required");
-				$this->form_validation->set_rules($quantity_available, "Jumlah Stok", "required|integer");
-			}
 		} 
 		
 		if ($this->form_validation->run() == TRUE)
